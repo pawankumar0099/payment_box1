284c0c8c82de992a47a349bf37e9540f497c75f0
Nishant Tanwar <nishant777000777@gmail.com>
2023-06-01 17:12:16 +0530

    new ui

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -75,34 +75,54 @@ class _CalculatorState extends State<Calculator> {
   }
 
 //method to create button
-  Widget buildButton(
-      String buttonText, double buttonHeight, Color buttonColor) {
+  Widget buildButton(String buttonText, double buttonHeight, Color buttonColor,
+      final buttonstyle) {
     return Container(
       height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
-      color: buttonColor,
-      child: TextButton(
+      child: ElevatedButton(
           style: TextButton.styleFrom(
+            backgroundColor: buttonColor,
             padding: const EdgeInsets.all(16.0),
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(0.0),
-                side: const BorderSide(
-                    color: Colors.white, width: 1, style: BorderStyle.solid)),
+              borderRadius: BorderRadius.circular(12),
+            ),
           ),
           onPressed: () => buttonPressed(buttonText),
           child: Text(
             buttonText,
-            style: const TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.normal,
-                color: Colors.white),
+            style: buttonstyle,
+          )),
+    );
+  }
+
+  Widget buildButtonNum(String buttonText, double buttonHeight) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.1 * buttonHeight,
+      child: ElevatedButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(16.0),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          onPressed: () => buttonPressed(buttonText),
+          child: Text(
+            buttonText,
+            style:
+                const TextStyle(fontSize: 30.0, fontWeight: FontWeight.normal),
           )),
     );
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: theme.colorScheme.primaryContainer,
         centerTitle: true,
         title: const Text("Payment Box"),
       ),
@@ -113,7 +133,9 @@ class _CalculatorState extends State<Calculator> {
             padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
             child: Text(
               equation,
-              style: TextStyle(fontSize: equationFontSize),
+              style: TextStyle(
+                  color: theme.colorScheme.primaryContainer,
+                  fontSize: equationFontSize),
             ),
           ),
           Container(
@@ -121,89 +143,92 @@ class _CalculatorState extends State<Calculator> {
             padding: const EdgeInsets.fromLTRB(10, 30, 10, 0),
             child: Text(
               result,
-              style: TextStyle(fontSize: resultFontSize),
+              style: TextStyle(
+                  color: theme.colorScheme.primary, fontSize: resultFontSize),
             ),
           ),
           const Expanded(
             child: Divider(),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                width: MediaQuery.of(context).size.width * .75,
-                child: Table(
-                  children: [
-                    TableRow(children: [
-                      buildButton("C", 1, Colors.redAccent),
-                      buildButton("⌫", 1, Colors.blue),
-                      buildButton("÷", 1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton("7", 1, Colors.black54),
-                      buildButton("8", 1, Colors.black54),
-                      buildButton("9", 1, Colors.black54),
-                    ]),
-                    TableRow(children: [
-                      buildButton("4", 1, Colors.black54),
-                      buildButton("5", 1, Colors.black54),
-                      buildButton("6", 1, Colors.black54),
-                    ]),
-                    TableRow(children: [
-                      buildButton("1", 1, Colors.black54),
-                      buildButton("2", 1, Colors.black54),
-                      buildButton("3", 1, Colors.black54),
-                    ]),
-                    TableRow(children: [
-                      buildButton(".", 1, Colors.black54),
-                      buildButton("0", 1, Colors.black54),
-                      buildButton("00", 1, Colors.black54),
-                    ]),
-                  ],
+          Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .75,
+                  child: Table(
+                    children: [
+                      TableRow(children: [
+                        buildButton("C", 1, theme.colorScheme.primary, style),
+                        buildButtonNum("⌫", 1),
+                        buildButtonNum("÷", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum("7", 1),
+                        buildButtonNum("8", 1),
+                        buildButtonNum("9", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum("4", 1),
+                        buildButtonNum("5", 1),
+                        buildButtonNum("6", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum("1", 1),
+                        buildButtonNum("2", 1),
+                        buildButtonNum("3", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum(".", 1),
+                        buildButtonNum("0", 1),
+                        buildButtonNum("00", 1),
+                      ]),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Table(
-                  children: [
-                    TableRow(children: [
-                      buildButton("×", 1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton("-", 1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton("+", 1, Colors.blue),
-                    ]),
-                    TableRow(children: [
-                      buildButton("=", 1, Colors.redAccent),
-                    ]),
-                    TableRow(children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.1 * 1,
-                        color: Colors.white,
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                              padding: const EdgeInsets.all(16.0),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(0.0),
-                                  side: const BorderSide(
-                                      color: Colors.white,
-                                      width: 1,
-                                      style: BorderStyle.solid)),
-                            ),
-                            onPressed: () => buttonPressed("QR"),
-                            child: const Icon(
-                              qr_code_scanner_rounded,
-                              color: Colors.black,
-                              size: 70.0,
-                            )),
-                      )
-                    ]),
-                  ],
-                ),
-              )
-            ],
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.25,
+                  child: Table(
+                    children: [
+                      TableRow(children: [
+                        buildButtonNum("×", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum("-", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum("+", 1),
+                      ]),
+                      TableRow(children: [
+                        buildButtonNum("=", 1),
+                      ]),
+                      TableRow(children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height * 0.1 * 1,
+                          color: Theme.of(context).colorScheme.primary,
+                          child: TextButton(
+                              style: TextButton.styleFrom(
+                                padding: const EdgeInsets.all(16.0),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(0.0),
+                                    side: const BorderSide(
+                                        color: Colors.white,
+                                        width: 1,
+                                        style: BorderStyle.solid)),
+                              ),
+                              onPressed: () => buttonPressed("QR"),
+                              child: const Icon(
+                                qr_code_scanner_rounded,
+                                color: Colors.white,
+                                size: 70.0,
+                              )),
+                        )
+                      ]),
+                    ],
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
