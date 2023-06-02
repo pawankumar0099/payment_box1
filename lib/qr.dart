e98a4a596df99c3b3ef89774fd49c6c6c4868c5b
Nishant Tanwar <nishant777000777@gmail.com>
2023-06-02 13:26:40 +0530

    qr

--- lib/qr.dart
+++ lib/qr.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'qrdata.dart';
+
 class QR extends StatefulWidget {
   String data;
   QR({required this.data});
@@ -11,25 +12,27 @@ class QR extends StatefulWidget {
 }
 
 class _QRState extends State<QR> {
-
-  String TTS="Recieved Payment of ${QrData.amount} rupees";
+  String TTS = "Recieved Payment of ${QrData.amount} rupees";
   final FlutterTts flutterTts = FlutterTts();
   Future<void> speak(String text) async {
     await flutterTts.setLanguage('en-US'); // Set the language
     await flutterTts.setPitch(1.0); // Set the pitch (1.0 is the default)
     await flutterTts.setVolume(1.0); // Set the volume (1.0 is the default)
-    await flutterTts.setSpeechRate(0.5); // Set the speech rate (1.0 is the default)
+    await flutterTts
+        .setSpeechRate(0.5); // Set the speech rate (1.0 is the default)
 
     await flutterTts.speak(text);
   }
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         title: Text("Payment Box"),
+        backgroundColor: theme.colorScheme.primaryContainer,
       ),
       body: Center(
         child: Column(
@@ -41,20 +44,21 @@ class _QRState extends State<QR> {
               size: 300,
             ),
             TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  padding: const EdgeInsets.all(16.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(0.0),
-                      side: const BorderSide(
-                          color: Colors.white, width: 1, style: BorderStyle.solid)),
-                ),
-              onPressed: ()=> {
-                  speak(TTS),
-                  Navigator.pushNamed(context, "/payemntstatus"),
-              }
-            ,
-              child: Text("click",style: TextStyle(color: Colors.black)),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.blue,
+                padding: const EdgeInsets.all(16.0),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(0.0),
+                    side: const BorderSide(
+                        color: Colors.white,
+                        width: 1,
+                        style: BorderStyle.solid)),
+              ),
+              onPressed: () => {
+                speak(TTS),
+                Navigator.pushNamed(context, "/payemntstatus"),
+              },
+              child: Text("click", style: TextStyle(color: Colors.black)),
             ),
           ],
         ),
